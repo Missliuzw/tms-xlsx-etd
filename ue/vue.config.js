3b8f03a05a034d4291ee424c8cdbd94bd6c34ba8
liuzhiwei <liuzhiwei@ctsi.com.cn>
2019-11-18 16:29:43 +0800

    指定build路径

--- ue/vue.config.js
+++ ue/vue.config.js
@@ -2,6 +2,11 @@
 const target = 'http://192.168.102.110:3530'
 module.exports = {
   // publicPath: '/etd/api',
+  // 基本路径
+  publicPath: "/etdue/",
+
+  // 输出文件目录
+  outputDir: "./public/etdue",
   devServer: {
     proxy: target
   }
