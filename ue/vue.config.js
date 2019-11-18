a21d916a92cf7618c2d553934fd887dc9774683b
liuzhiwei <liuzhiwei@ctsi.com.cn>
2019-11-18 17:01:16 +0800

    配置项修复

--- ue/vue.config.js
+++ ue/vue.config.js
@@ -3,7 +3,7 @@ const target = 'http://192.168.102.110:3530'
 module.exports = {
   // publicPath: '/etd/api',
   // 基本路径
-  publicPath: "/etdue/",
+  publicPath: "",
 
   // 输出文件目录
   outputDir: "../back/public/etdue",
